469a89cd7c0f0321768cf2e979fa14be777072ca
Phuc Truong <tvphuc279@gmail.com>
2019-03-22 16:41:34 +0700

    Update compiler.rb
    Fix error on Rails 5.2: undefined method `build_attributes' for class `#<Class:Haml::Compiler>' (NameError)

--- lib/hamlbars/ext/compiler.rb
+++ lib/hamlbars/ext/compiler.rb
@@ -51,7 +51,7 @@ module Hamlbars
 
             handlebars_rendered_attributes
           end
-          alias build_attributes_without_handlebars_attributes build_attributes
+          # alias build_attributes_without_handlebars_attributes build_attributes
           alias build_attributes build_attributes_with_handlebars_attributes
         end
       end
